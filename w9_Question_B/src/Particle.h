e6c8103ce98239a92f66dddc3b8e608f11d24707
qishisuicong <qishisuicong@gmail.com>
2016-11-03 15:33:00 -0400

    questionb

---
+++ w9_Question_B/src/Particle.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ofMain.h"
+
+class Particle {
+public:
+    Particle();
+    
+    void setInit(ofPoint _pos, ofPoint _vel = ofPoint(0,0));
+    
+    float   getRadius();
+    ofPoint getPosition();
+    ofPoint getVelocity();
+    
+    void addForce(ofPoint _force);
+    void mouse(int x, int y);
+    
+    void addRepulsionForce(ofPoint _pos, float _rad, float _scale);
+    void addAttractionForce(ofPoint _pos, float _rad, float _scale);
+    void addFlockingForce(vector<Particle> &_particles, float _neighborhoodRadius, float _alignmentScale = 0.33, float _separationScale = 0.33, float _cohesionScale = 0.33);
+    
+    void bounding();
+    void infiniteWalls();
+    
+    void update();
+    void draw();
+    ofVec2f mAcc;
+    
+private:
+    ofPoint pos;
+    ofPoint vel;
+    ofPoint acc;
+    
+    vector<ofPoint> tail;
+    
+    float radius;
+    float damp;
+};
---
+++ w9_Question_B/src/ofApp.cpp
@@ -0,0 +1,109 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    vf.setup(ofGetWidth(), ofGetHeight(), 20);
+//    vf.setNoise();
+
+    for (int i = 0; i < 100; i++) {
+        Particle p;
+        p.setInit(ofPoint(ofRandom(ofGetWidth()),ofRandom(ofGetHeight())));
+        particles.push_back(p);
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+    vf.setNoise(ofGetElapsedTimef());
+    vf.update();
+    
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i].addForce( vf.getForce(particles[i].getPosition()) * 0.1 );
+        particles[i].update();
+        particles[i].infiniteWalls();
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(0);
+    
+    
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i].draw();
+        
+    }
+    ofDrawBitmapString("Click Your Mouse", 100, 100);
+    
+
+    //the new way of writing for loops "for each loop"
+    //& means reference 
+//    for(auto & particle: particles){
+//        particle.draw();
+//    }
+    
+    vf.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i].mouse(x,y);
+    }
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
